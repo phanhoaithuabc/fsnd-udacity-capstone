ee9dfdf50b90bce22fb9f14669f50508d2a4ce72
Git <git@github.com>
2024-05-09 10:15:16 +0700

    fix ok

--- api.py
+++ api.py
@@ -4,8 +4,8 @@ from sqlalchemy import exc
 import json
 from flask_cors import CORS
 
-from .database.models import db_drop_and_create_all, setup_db, Drink
-from .auth.auth import AuthError, requires_auth
+from models import db_drop_and_create_all, setup_db, Drink
+from auth import AuthError, requires_auth
 
 AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
 ALGORITHMS = ['RS256']
